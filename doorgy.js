339e342219a44ab4ade5b678c95f8a232aa0038f
Anthony Kung <hi@anth.dev>
2021-04-27 08:20:52 -0700

    improve core

--- doorgy.js
+++ doorgy.js
@@ -57,12 +57,12 @@ IR_INT.watch((err, value) => {
   }
   else if (value) {
     // Inform Servo Unit montion is detected
-    console.log('IR Sense', value);
+    console.log('IR_INT Sense', value);
     LED_ERR.writeSync(1);
   }
   else {
     // Inform Servo Unit no motion is detected
-    console.log('IR Not Sense', value);
+    console.log('IR_INT Not Sense', value);
     LED_ERR.writeSync(0);
   }
 });
@@ -74,12 +74,12 @@ IR_EXT.watch((err, value) => {
   }
   else if (value) {
     // Inform Servo Unit montion is detected
-    console.log('IR Sense', value);
+    console.log('IR_EXT Sense', value);
     LED_ERR.writeSync(1);
   }
   else {
     // Inform Servo Unit no motion is detected
-    console.log('IR Not Sense', value);
+    console.log('IR_EXT Not Sense', value);
     LED_ERR.writeSync(0);
   }
 });
@@ -91,6 +91,17 @@ PSH_BTN1.watch((err, value) => {
   }
   else if (value) {
     ctrlSig = 0;
+    LED_PWR.writeSync(0);
+    LED_PWR.writeSync(0);
+    LED_PWR.writeSync(0);
+    LED_PWR.writeSync(0);
+    IR_INT.unexport();
+    IR_EXT.unexport();
+    LED_PWR.unexport();
+    LED_NET.unexport();
+    LED_LCK.unexport();
+    LED_ERR.unexport();
+    exit(0);
     let cmd = spawn('shutdown -h now', [], { stdio: 'inherit' });
     cmd.on('error', (error) => {
       console.log(anth.red, 'ACLI Error:', error, anth.ori);
@@ -104,6 +115,11 @@ PSH_BTN1.watch((err, value) => {
   }
 });
 
+function turnOffRed() {
+  console.log('Lock Release');
+  LED_LCK.writeSync(0);
+}
+
 // Simulate Lock On
 PSH_BTN2.watch((err, value) => {
   if (err) {
@@ -113,6 +129,7 @@ PSH_BTN2.watch((err, value) => {
     // Turn on Lock indicator
     console.log('Lock Sense', value);
     LED_LCK.writeSync(1);
+    setTimeout(turnOffRed, 2000);
   }
   else {
     // Turn off Lock indicator
