20121331e24cfc385c9e01fb60c4a246072748fd
Anthony Kung <hi@anth.dev>
2021-05-26 10:57:01 -0700

    added dummy config

---
+++ resources/config.js
@@ -0,0 +1,10 @@
+// Just to be nice
+'use strict';
+
+// Modules
+const { exec, spawn } = require('child_process');
+const anth = require('./anthonian.js');
+const fs = require("fs");
+const path = require('path');
+const fse = require('fs-extra');
+
